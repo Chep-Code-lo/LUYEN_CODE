e886580b19d7fb0f946aa9148aff86c6ab9bfd33
Chep-Code-lo <24162008@student.hcmute.edu.vn>
2024-10-18 22:17:24 +0700

    bai 1

--- HOC_DP/DCTDN1.cpp
+++ HOC_DP/DCTDN1.cpp
@@ -1,17 +1,30 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include<bits/stdc++.h>
+#define ll long long
+#define N (int)2e5+5
+#define FOR(i,l,r) for(int i = l ; i <= r ; i++)
+#define FOD(i,r,l) for(int i = r ; i >= l ; i--)
+#define faster ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
-int n, ans, a[1005];
-vector<int>dp(1005, 1e9);
-int main(){
+int n, a[N], res;
+int main()
+{
     cin >> n;
-    for(int i=1; i<=n; ++i) cin >> a[i];
-    dp[0] = -1e9;
-    for(int i=1; i<=n; ++i){
-        int k = lower_bound(dp.begin(), dp.end(), a[i]) - dp.begin();
-        dp[k] = a[i];
-        ans = max(ans, k);
+    FOR(i,1,n) cin >> a[i];
+    vector<int>f(n+1),b(n+1,INT_MAX);
+    b[0] = INT_MIN;
+    FOR(i,1,n){
+        int k = lower_bound(b.begin(), b.end(), a[i] ) - b.begin();
+        b[k] = a[i];
+        f[i] = k;
+        res = max(res, k);
     }
-    cout << ans << "\n";
+    cout << res << '\n';
+    vector<int>v;
+    for(int i=n; i>=1; i--){
+        if(f[i] == res)
+            v.push_back(i), res--;
+    }
+    //for(int i=v.size()-1; i>=0; i--)
+        //cout << v[i] << ' ';
+  
 }
