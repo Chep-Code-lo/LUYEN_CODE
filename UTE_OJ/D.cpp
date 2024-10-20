f262933b3ace071cfd12ae7fbc4a9f78d29fa75f
Chep-Code-lo <24162008@student.hcmute.edu.vn>
2024-10-20 21:27:04 +0700

    bai 4

---
+++ UTE_OJ/D.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int n, a[1000006];
+int main(){
+    cin >> n;
+    for(int i=1; i<=n; ++i) cin >> a[i];
+    for(int i=n; i>=1; --i)
+        if(i%2 != 0)
+            cout << a[i] << " ";
+}
