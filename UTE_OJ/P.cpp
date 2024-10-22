f30a92cacbb63a13341104fac6d84c476f1883a2
Chep-Code-lo <24162008@student.hcmute.edu.vn>
2024-10-22 12:04:31 +0700

    bai 15

---
+++ UTE_OJ/P.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int n, a[1003];
+int main(){
+    cin >> n;
+    for(int i=0; i<n; ++i) cin >> a[i];
+    sort(a, a+n);
+    for(int i=0; i<n; ++i) cout << a[i] << " ";
+}
