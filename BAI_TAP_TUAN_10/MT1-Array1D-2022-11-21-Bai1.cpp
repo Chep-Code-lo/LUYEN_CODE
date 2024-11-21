a441b29c9e3ae3db638457bdb3caa880def2e267
Chep-Code-lo <24162008@student.hcmute.edu.vn>
2024-11-21 22:13:02 +0700

    f

--- BAI_TAP_TUAN_10/MT1-Array1D-2022-11-21-Bai1.cpp
+++ BAI_TAP_TUAN_10/MT1-Array1D-2022-11-21-Bai1.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
 using namespace std;
-int n, maxx, pos_1, pos_2, a[103];
-int main() {
+int n, maxx = -1e9, pos_1, pos_2, a[103];
+int main(){
     cin >> n;
-    for(int i=0; i<n; ++i) cin >> a[i];
-    maxx = a[0];
-    for (int i = 1; i < n; ++i)
-        if (a[i] > maxx) {
-            maxx = a[i];
-            pos_1 = pos_2 = i; 
-        } else if (a[i] == maxx)
-            pos_2 = i; 
+    for(int i=0; i<n; ++i) cin >> a[i], maxx = max(maxx, a[i]);
+    for(int i=0; i<n; ++i)
+        if(a[i] == maxx){
+            pos_1 = i;
+            break;
+        }
+    for(int i=n-1; i>=0; --i)
+        if(a[i] == maxx){
+            pos_2 = i;
+            break;
+        }
     cout << pos_2 - pos_1;
 }
