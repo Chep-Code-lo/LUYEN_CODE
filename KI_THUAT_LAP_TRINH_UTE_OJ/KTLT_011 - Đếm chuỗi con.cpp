43063ef4449619bd503c592d433c9240342947c1
Thanh-Chep-Code-Lo <bomay0080@gmail.com>
2025-02-08 22:33:34 +0700

    ...

--- KI_THUAT_LAP_TRINH_UTE_OJ/KTLT_011 - Đếm chuỗi con.cpp
+++ KI_THUAT_LAP_TRINH_UTE_OJ/KTLT_011 - Đếm chuỗi con.cpp
@@ -1,59 +1,18 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-int n;
-
-int cnt;
-
-int s;
-
-void printSubset(vector<int> &arr, vector<int> &subset, int sum, int start) {
-    if (sum == 0)
-    {
-        for (int i = 0; i < subset.size(); i++)
-        {
-            cout << subset[i] << " ";
-        }
-        cout << endl;
+int n, cnt, a[21];
+long long s;
+void backtrack(int pos, long long sum, int temp){
+    if(pos == n){
+        if(temp && sum == s)    cnt++;
         return;
     }
-    if (start == arr.size() || sum < 0)
-    {
-        return;
-    }
-    subset.push_back(arr[start]);
-    printSubset(arr, subset, sum - arr[start], start + 1);
-    subset.pop_back();
-    printSubset(arr, subset, sum, start + 1);
+    backtrack(pos+1, sum, temp);
+    backtrack(pos+1, sum+a[pos], 1);
 }
-void f(vector<int> &arr, int i, int currSum)
-{
-    if (currSum == s)
-    {
-        cnt++;
-        return;
-    }
-    if (currSum < s && i < n)
-    {
-        f(arr, i + 1, currSum + arr[i]);
-
-        f(arr, i + 1, currSum);
-    }
-}
-int main()
-{
-    cin>>n>>s;
-    cnt = 0;
-    vector<int> arr(n);
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-    vector<int> subset;
-    f(arr,0,0);
-    if( cnt == 1 )
-    {
-         printSubset(arr, subset, s, 0);
-    }
-    else
-        cout<<cnt;
-
-    return 0;
+int main(){
+    cin >> n >> s;
+    for(int i=0; i<n; ++i)  cin >> a[i];
+    backtrack(0,0,0);
+    cout << cnt;
 }
