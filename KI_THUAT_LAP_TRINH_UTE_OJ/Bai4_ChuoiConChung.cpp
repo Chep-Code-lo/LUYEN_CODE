f9a85e8c3c4d881ce19fbeec8b5a825849f6c4b9
Thanh-Chep-Code-Lo <bomay0080@gmail.com>
2025-02-08 23:07:57 +0700

    ..

--- KI_THUAT_LAP_TRINH_UTE_OJ/Bai4_ChuoiConChung.cpp
+++ KI_THUAT_LAP_TRINH_UTE_OJ/Bai4_ChuoiConChung.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
-#include<algorithm>
+#include<iostream>
+#include<string>
+#include<bits/stdc++.h>
 using namespace std;
-int n1, n2, f[2005][2005];
-string s1, s2;
-int main()
-{
-    cin >> s1 >> s2;
-    n1 = s1.size(), n2= s2.size();
-    s1 = "#" + s1;
-    s2 = "#" + s2;
-    for(int i=1; i<=n1; ++i)
-        for(int j=1; j<=n2; ++j){
-            if(s1[i] == s2[j]) f[i][j] = f[i-1][j-1] + 1;
+int f[2001][2001];
+string a, b;
+int main(){
+    getline(cin, a);
+    getline(cin, b);
+    a = "#" + a;
+    b = "#" + b;
+    for(int i=1; i<a.size(); ++i)
+        for(int j=1; j<b.size(); ++j){
+            if(a[i] == b[j])    f[i][j] = f[i-1][j-1] + 1;
             f[i][j] = max({f[i][j], f[i-1][j], f[i][j-1]});
-        } 
-    cout << f[n1][n2];   
+        }
+    cout << f[a.size()-1][b.size()-1];
 }
-    
