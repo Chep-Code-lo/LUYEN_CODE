750ddb04fa0e6ab8beb79e0ee64df9b88e38f81e
Thanh-Chep-Code-Lo <bomay0080@gmail.com>
2025-02-14 23:28:05 +0700

    ...

--- test.cpp
+++ test.cpp
@@ -1,273 +1,161 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#define ll long long
 using namespace std;
-const int base = 1e9;
-const int base_digits = 9;
-struct bigint{
-	vector<int>a;
-	int sign;
-	bigint(): sign(1){ }
-	bigint(long long v) { *this = v; }
-	bigint(const string &s) { read(s); }
-	void operator=(const bigint &v) { sign = v.sign; a = v.a; }
-    bigint operator/(const bigint &v) const{return divmod(*this, v).first;}
-    bigint operator%(const bigint &v) const{return divmod(*this, v).second;}
-    bigint operator/(int v) const{bigint res = *this; res /=v; return res;}
-    void operator*=(const bigint &v) {*this = *this*v;}
-    void operator+=(const bigint &v) {*this = *this+v;}
-    void operator-=(const bigint &v) {*this = *this-v;}
-    void operator/=(const bigint &v) {*this = *this/v;}
-    bool operator>(const bigint &v) const{return v<*this;}
-    bool operator<=(const bigint &v) const{return !(v<*this);}
-    bool operator>=(const bigint &v) const{return !(*this<v);}
-    bool operator==(const bigint &v) const{return !(*this<v) && !(v<*this);}
-    bool operator!=(const bigint &v) const{return *this<v || v<*this;}
-    bool operator<(const bigint &v) const{
-        if(sign != v.sign)	return sign<v.sign;
-        if(a.size() != v.a.size())	return a.size()*sign < v.a.size()*v.sign;
-        for(int i=a.size()-1; i>+0; --i)
-            if(a[i] != v.a[i])	return a[i]*sign < v.a[i]*sign;
+struct date
+{
+    int ngay;
+    int thang;
+    int nam;
+};
+void nhap(date &x)
+{
+    cin >> x.ngay;
+    cin >> x.thang;
+    cin >> x.nam;
+}
+bool namnhuan(date x)
+{
+    return (x.nam % 400 == 0) || (x.nam % 4 == 0 && x.nam % 100 != 0);
+}
+bool check(date x)
+{
+    if (x.thang == 2)
+    {
+        if (namnhuan(x) && x.ngay > 29)
+            return false;
+        if (!namnhuan(x) && x.ngay > 28)
+            return false;
+    }
+    else if (x.thang == 1 || x.thang == 3 || x.thang == 5 || x.thang == 7 || x.thang == 8 || x.thang == 10 || x.thang == 12)
+    {
+        if (x.ngay > 31)
+            return false;
+    }
+    else
+    {
+        if (x.ngay > 30)
+            return false;
+    }
+    if (x.thang > 12 || x.thang < 0)
         return false;
-    }
-    bool isZero() const{return a.empty() || (a.size() == 1 && !a[0]);}
-    void trim(){
-        while(!a.empty() && !a.back())	a.pop_back();
-        if(a.empty())	sign=1;
-    }
-    bigint abs() const{
-        bigint res = *this;
-        res.sign *= res.sign;
-        return res;
-    }
-	void operator=(long long v){
-		sign = 1;
-		if(v < 0) 	sign = -1, v = -v;
-		for(; v>0; v=v/base)	a.push_back(v%base);
-    }
-    bigint operator+(const bigint &v) const{
-        if(sign == v.sign){
-            bigint res = v;
-            for(int i=0, carry=0; i<(int)max(a.size(), v.a.size()) || carry; ++i){
-                if(i == (int) res.a.size())	res.a.push_back(0);
-                res.a[i] += carry + (i<(int)a.size() ? a[i] : 0);
-                carry = res.a[i] >= base;
-                if(carry)	res.a[i] -= base;
-            }
-            return res;
-        }
-        return *this - (-v);
-    }
-    bigint operator-(const bigint &v) const{
-        if(sign == v.sign){
-            if(abs() >= v.abs()){
-                bigint res = *this;
-                for(int i=0, carry=0; i<(int)v.a.size() || carry; ++i){
-                    res.a[i] -= carry + (i<(int)v.a.size() ? v.a[i] : 0);
-                    carry = res.a[i] < 0;
-                    if(carry) 	res.a[i] += base;	
-                }
-                res.trim();
-                return res;
-            }
-            return -(v - *this);
-        }
-        return *this + (-v);
-    }
-    bigint operator-() const{
-        bigint res = *this;
-        res.sign = -sign;
-        return res;
-    }
-     int operator%(int v) const{
-        if(v<0) v = -v;
-        int m=0;
-        for(int i=a.size()-1; i>=0; --i)	m = (a[i] + m*(long long)base)%v;
-        return m*sign;
-    }
-    void operator/=(int v){
-        if(v<0) sign=-sign, v=-v;
-        for(int i=(int)a.size()-1, rem=0; i>=0; --i){
-            long long cur = a[i] + rem*(long long)base;
-            a[i] = (int)(cur/v), rem = (int)(cur%v);
-        }
-        trim();
-    }
-    bigint operator*(const bigint &v) const{
-        bigint res;
-        res.sign = sign * v.sign;
-        res.a.resize(a.size() + v.a.size());
-        for(int i=0; i<(int)a.size(); ++i){
-            long long carry = 0;
-            for(int j=0; j<(int)v.a.size() || carry; ++j){
-                long long cur = res.a[i + j] + (long long)a[i] * (j<(int)v.a.size() ? v.a[j] : 0) + carry;
-                res.a[i + j] = cur % base;
-                carry = cur / base;
-            }
-        }
-        res.trim();
-        return res;
-    }
-    friend pair<bigint, bigint>divmod(const bigint &a1, const bigint &b1){
-        int norm = base/(b1.a.back() + 1);
-        bigint q, r, a=a1.abs()*norm, b=b1.abs()*norm;
-        q.a.resize(a.a.size());
-        for(int i=a.a.size()-1; i>=0; i--){
-            r *= base, r+= a.a[i];
-            int s1 = r.a.size() <= b.a.size() ? 0 : r.a[b.a.size()];
-            int s2 = r.a.size() <= b.a.size() - 1 ? 0 : r.a[b.a.size() - 1];
-            int d = ((long long)base*s1+s2) / b.a.back();
-            r -= b*d;
-            while(r < 0)    r+=b, --d;
-            q.a[i] = d;
-        }
-        q.sign = a1.sign*b1.sign;
-        r.sign = a1.sign;
-        q.trim();
-        r.trim();
-        return make_pair(q, r/norm);
-    }
-    //Chuyển bigint sang long long
-    long long longValue() const{
-        long long res=0;
-        for(int i=a.size()-1; i>=0; i--)	res = res*base + a[i];
-        return res*sign;
-    }
-    friend bigint gcd(const bigint &a, const bigint &b){return b.isZero() ? a : gcd(b, a%b);}
-    friend bigint lcm(const bigint &a, const bigint &b){return a/gcd(a, b)*b;}
-    void read(const string &s){
-        sign=1;
-        a.clear();
-        int pos=0;
-        while(pos<(int)s.size() && (s[pos] == '-' || s[pos] == '+')){
-            if(s[pos] == '-')	sign = -sign;
-            ++pos;
-        }
-        for(int i=s.size()-1; i>=pos; i-=base_digits){
-            int x=0;
-            for(int j=max(pos, i-base_digits+1); j<=i; j++)	x=x*10 + s[j] - '0';
-            a.push_back(x);
-        }
-        trim();
-    }
-    friend istream& operator>>(istream &stream, bigint &v){
-        string s;
-        stream >> s;
-        v.read(s);
-        return stream;
-    }
-    friend ostream& operator<<(ostream &stream, const bigint &v){
-        if (v.sign == -1)
-            stream << '-';
-        stream << (v.a.empty() ? 0 : v.a.back());
-        for(int i=(int)v.a.size()-2; i>=0; --i)
-            stream << setw(base_digits) << setfill('0') << v.a[i];
-        return stream;
-    }
-};
-struct PhanSo{
-    int tu, mau;
-    void rut_gon(){
-        bigint gcd1 = __gcd(tu, mau);
-        tu /= gcd1;
-        mau /= gcd1;
-        if(mau < 0){
-            tu = -tu;
-            mau = -mau;
-        }
-    }
-    PhanSo rev() const{
-        int mau_temp = tu;
-        int tu_temp = mau;
-        if(mau_temp < 0){
-            mau_temp = -mau_temp;
-            tu_temp = -tu_temp;
-        }
-        return {tu_temp, mau_temp};
-    }
-    bool operator>(const PhanSo &other) const{
-        return tu * other.mau > mau * other.tu;
-    }
-    PhanSo operator+(const PhanSo &other) const{
-        int newTu = tu * other.mau + other.tu * mau;
-        int newMau = mau * other.mau;
-        PhanSo result = {newTu, newMau};
-        result.rut_gon();
-        return result;
-    }
-    PhanSo operator*(const PhanSo &other) const{
-        bigint newTu = tu * other.tu;
-        bigint newMau = mau * other.mau;
-        PhanSo result = {newTu, newMau};
-        result.rut_gon();
-        return result;
-    }
-    void print() const{
-        cout << tu << " " << mau << "\n";
-    }
-};
-void in(PhanSo &ps){
-    cin >> ps.tu >> ps.mau;
-    ps.rut_gon();
+    if (x.nam < 0)
+        return false;
+    return true;
 }
-void input(vector<PhanSo> &ds, int n){
-    for(int i = 0; i < n; ++i){
-        PhanSo ps;
-        in(ps);
-        ds.push_back(ps);
+ll tong_ngay(date x)
+{
+    ll kq = 0;
+    if (x.thang == 1)
+        return x.ngay;
+    kq = 31;
+    if (x.thang == 2)
+        return x.ngay + kq;
+    if (x.thang > 2 && namnhuan(x))
+    {
+        kq += 29;
+        if (x.thang == 3)
+            return x.ngay + kq;
+        kq += 31;
+        if (x.thang == 4)
+            return x.ngay + kq;
+        kq += 30;
+        if (x.thang == 5)
+            return x.ngay + kq;
+        kq += 31;
+        if (x.thang == 6)
+            return x.ngay + kq;
+        kq += 30;
+        if (x.thang == 7)
+            return x.ngay + kq;
+        kq += 31;
+        if (x.thang == 8)
+            return x.ngay + kq;
+        kq += 31;
+        if (x.thang == 9)
+            return x.ngay + kq;
+        kq += 30;
+        if (x.thang == 10)
+            return x.ngay + kq;
+        kq += 31;
+        if (x.thang == 11)
+            return x.ngay + kq;
+        kq += 30;
+        if (x.thang == 12)
+            return x.ngay + kq;
+    }
+    else
+    {
+        kq += 28;
+        if (x.thang == 3)
+            return x.ngay + kq;
+        kq += 31;
+        if (x.thang == 4)
+            return x.ngay + kq;
+        kq += 30;
+        if (x.thang == 5)
+            return x.ngay + kq;
+        kq += 31;
+        if (x.thang == 6)
+            return x.ngay + kq;
+        kq += 30;
+        if (x.thang == 7)
+            return x.ngay + kq;
+        kq += 31;
+        if (x.thang == 8)
+            return x.ngay + kq;
+        kq += 31;
+        if (x.thang == 9)
+            return x.ngay + kq;
+        kq += 30;
+        if (x.thang == 10)
+            return x.ngay + kq;
+        kq += 31;
+        if (x.thang == 11)
+            return x.ngay + kq;
+        kq += 30;
+        if (x.thang == 12)
+            return x.ngay + kq;
     }
 }
-
-PhanSo find_max(const vector<PhanSo> &ds){
-    return *max_element(ds.begin(), ds.end(), 
-        [](const PhanSo &a, const PhanSo &b)
-            {return !(a > b);} 
-    );
+ll dem_nam_nhuan(date x)
+{
+    return x.nam / 4 - x.nam / 100 + x.nam / 400;
 }
-PhanSo sum(const vector<PhanSo> &ds){
-    PhanSo sum = {0, 1};
-    for (const auto &ps : ds) {
-        sum = sum + ps;
+ll ketqua(date x)
+{
+    ll kq = 0;
+    x.nam--;
+    ll n = dem_nam_nhuan(x);
+    kq += (n) * 366;
+    kq += (x.nam - n) * 365;
+    kq += tong_ngay(x) - 1;
+    return kq;
+}
+int main()
+{
+    date tg1;
+    date tg2;
+    nhap(tg1);
+    nhap(tg2);
+    if (!check(tg1))
+    {
+        cout << "-1";
+        return 0;
     }
-    return sum;
-}
-PhanSo product(const vector<PhanSo> &ds){
-    PhanSo product = {1, 1};
-    for (const auto &ps : ds) {
-        product = product * ps;
+    if (!check(tg2))
+    {
+        cout << "-1";
+        return 0;
     }
-    return product;
-}
-void print_rev(const vector<PhanSo> &ds){
-    for(const auto &ps : ds){
-        PhanSo rev = ps.rev();
-        cout << rev.tu << " " << rev.mau << " ";
+    ll kq1 = ketqua(tg1);
+    ll kq2 = ketqua(tg2);
+    if (kq1 > kq2)
+    {
+        cout << kq1 - kq2;
+        return 0;
     }
+    else
+        cout << kq2 - kq1;
+    return 0;
 }
-int main(){
-    int n;  cin >> n;
-    vector<PhanSo> ds;
-    input(ds, n);
-    find_max(ds).print();
-    sum(ds).print();
-    product(ds).print();
-    print_rev(ds);
-}
-
-/*18 
--4 -7 
-6 6 
--3 5 
-4 -10 
--7 -9 
-4 7 
--6 -5 
-6 -1 
--6 10 
-1 -6 
--8 -2
-1 -7 
-3 1 
--4 5 
-2 6 
--4 9 
-10 -3 
--6 7*/
