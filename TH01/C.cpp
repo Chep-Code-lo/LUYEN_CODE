5b7076854f14f2e74edcc74d3a9ad7d033e7c31d
Thanh-Chep-Code-Lo <bomay0080@gmail.com>
2025-03-03 20:00:13 +0700

    .......

--- TH01/C.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int limit, maxx, a[1003];
-int main(){
-    cin >> limit;
-    for(int i=0; i<limit; ++i) cin >> a[i];
-    for(int i=0; i<limit-1; ++i)  maxx = max(maxx, abs(a[i+1] - a[i]));
-    cout << maxx << "\n";
-}
